ac15b0419a3d90f473a70f031640895435971a7d
JackWH <yukiakira259@gmail.com>
2022-10-13 01:23:30 +0700

    Add files via upload
    Signed-off-by: JackWH <yukiakira259@gmail.com>

---
+++ syndicai.py
@@ -0,0 +1,25 @@
+def __init__(self, config):
+    self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+
+def predict(self, payload):
+        """
+        Called once per request. Preprocesses the request payload (if necessary), 
+        runs inference, and postprocesses the inference output (if necessary).
+        Args:
+            payload: The request payload
+        Returns:
+            Prediction or a batch of predictions.
+        """
+
+        # Convert url image to PIL format
+        img = url_to_img(payload["url"])
+
+        # Run a model
+        results = self.model(img)
+
+        # Draw boxes
+        boxes = results.xyxy[0].cpu().numpy()
+        box_img = draw_box(img, boxes)
+
+        # Return an image in the base64 format
+        return img_to_bytes(box_img)
